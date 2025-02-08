05ff5cbdef2bac3052845a9247b36098c714c35f
Shibo Lyu <hi@lao.sb>
2025-02-09 06:20:39 +0800

    fix: ui translations

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,22 @@
 import { defineCollection } from "astro:content";
 import { docsLoader } from "@astrojs/starlight/loaders";
-import { docsSchema } from "@astrojs/starlight/schema";
+import { docsSchema, i18nSchema } from "@astrojs/starlight/schema";
 import { topicSchema } from "starlight-sidebar-topics/schema";
+
+import zhHans from "../node_modules/@astrojs/starlight/translations/zh-CN.json";
+import zhHant from "../node_modules/@astrojs/starlight/translations/zh-TW.json";
 
 export const collections = {
   docs: defineCollection({
     loader: docsLoader(),
     schema: docsSchema({ extend: topicSchema }),
+  }),
+  i18n: defineCollection({
+    // We're basing translations on written script not region.
+    loader: async () => ({
+      "zh-Hans": zhHans,
+      "zh-Hant": zhHant,
+    }),
+    schema: i18nSchema(),
   }),
 };
